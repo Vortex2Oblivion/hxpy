2460b93d2a821c974e5731263ae210d22bc1b04e
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-03-08 17:55:44 -0500

    HOLY SHIT IT FUCKING WORKS

--- test/flixel/source/PlayState.hx
+++ test/flixel/source/PlayState.hx
@@ -7,7 +7,7 @@ class PlayState extends FlxState
 	override public function create()
 	{
 		super.create();
-		trace(Python.VERSION);
+		trace("Python Ver: " + Python.VERSION);
 	}
 
 	override public function update(elapsed:Float)
