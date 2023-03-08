c00ee3c3d60fce122cd5dbc183bfcd6b6adf0aa1
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-03-08 17:22:54 -0500

    maybe this?

--- test/flixel/source/PlayState.hx
+++ test/flixel/source/PlayState.hx
@@ -1,12 +1,13 @@
 package;
 
 import flixel.FlxState;
-
+import hxpy.Python;
 class PlayState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+		trace(Python.VERSION);
 	}
 
 	override public function update(elapsed:Float)
