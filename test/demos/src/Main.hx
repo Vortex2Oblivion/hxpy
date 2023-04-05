18e5e65ec755b0029f1932700772670351b32d85
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-04-05 18:21:06 -0400

    aaaaa

--- test/demos/src/Main.hx
+++ test/demos/src/Main.hx
@@ -6,6 +6,6 @@ class Main {
 	public static function main():Void {
         trace("Python Ver: " + Python.VERSION);
 		Python.pythonInitialize();
-		Python.pythonRunSimpleString("Hello from embedded python!");
+		Python.pythonRunSimpleString("Hello World From Embedded Python!");
 	}
 }
