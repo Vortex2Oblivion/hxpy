f1ad6e9df454b9331bb97e3fa38e42eec79d0f7e
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2023-05-17 19:26:48 -0400

    remove trace

--- test/demos/pythonRunSimpleFile/src/Main.hx
+++ test/demos/pythonRunSimpleFile/src/Main.hx
@@ -10,7 +10,6 @@ class Main {
 	public static function main():Void {
 		//tracing some basic copyright and version information
         trace("Python Ver: " + Python.VERSION);
-		trace(Python.pythonGetCopyright());
 		PythonOpen.pythonInitialize(); // Initializes python
 		PythonFile.pythonRunSimpleFile(/*path to script file*/"script.py"); // runs code
 		PythonOpen.pythonFinalize(); //  closes python
