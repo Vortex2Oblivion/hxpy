09e72d5caa879e80c0e49914bee7ffe8f4e20ee1
Vortex <73261680+Vortex2Oblivion@users.noreply.github.com>
2024-01-08 11:23:52 -0500

    Update Main.hx

--- test/demos/running code from a file/src/Main.hx
+++ test/demos/running code from a file/src/Main.hx
@@ -1,14 +1,13 @@
 package;
 
 import hxpy.Python;
-import hxpy.Python.File;
 
 class Main {
 	public static function main():Void {
 		//initializes the python instance
 		Python.initialize();
 		 //runs code               path to script file
-		File.runSimpleFile("script.py");
+		Python.runSimpleFile("script.py");
 		//closes the python instance
 		Python.finalize();
 	}
