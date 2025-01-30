875e43152ff49e22c35f155f4bb118c4c8526092
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-01-30 18:03:08 -0500

    we are so back

--- hxpy/Python.hx
+++ hxpy/Python.hx
@@ -1,5 +1,10 @@
 package hxpy;
 
+import cpp.RawConstPointer;
+import cpp.NativeString;
+import cpp.Char;
+import cpp.ConstPointer;
+import cpp.RawPointer;
 import cpp.ConstCharStar;
 
 @:buildXml("<include name='${haxelib:hxpy}/hxpy/Build.xml' />")
@@ -20,13 +25,35 @@ extern class Python
     @:native('Py_Initialize')
 	static function initialize():Void;
 
+    @:native('Py_InitializeFromConfig')
+	static function initializeFromConfig(config:RawPointer<PyConfig>):PyStatus;
+
     @:native('PyRun_SimpleString')
 	static function runSimpleString(command:ConstCharStar):Int;
 
     @:native('PyRun_SimpleStringFlags')
-	static function runSimpleStringFlags(command:ConstCharStar):Int;
+	static function runSimpleStringFlags(command:ConstCharStar, flags:RawPointer<PyCompilerFlags>):Int;
+
+    @:native('PyConfig_Clear')
+	static function configClear(config:RawPointer<PyConfig>):Void;
+
+    @:native('Py_ExitStatusException')
+	static function exitStatusException(err:PyStatus):Void;
+
+    @:native('PyConfig_InitPythonConfig')
+	static function initConfig(config:RawPointer<PyConfig>):Void;
+
+	static inline function setBytesString(config:RawPointer<PyConfig>, config_str:RawConstPointer<WChar>, str:ConstCharStar):PyStatus{
+        return untyped __cpp__("PyConfig_SetBytesString({0}, const_cast<wchar_t**>({1}), {2})", config, RawPointer.addressOf(config_str), str);
+    };
+
+    @:native('PyStatus_Exception')
+	static function exception(err:PyStatus):Int;
 
     @:native('Py_Finalize')
 	static function finalize():Void;
+
+    @:native('Py_FinalizeEx')
+	static function finalizeEx():Int;
         
 }
--- test/src/Main.hx
+++ test/src/Main.hx
@@ -1,11 +1,42 @@
 package;
 
+import cpp.RawConstPointer;
+import hxpy.WChar;
+import hxpy.PyConfig;
+import hxpy.PyStatus;
 import hxpy.Python;
 
+using cpp.RawPointer;
+using hxpy.WChar;
+
 class Main {
+	static var status:PyStatus;
+	static var config:PyConfig;
+
 	static function main() {
-		Python.initialize();
-		Python.runSimpleString("print(\"hello world\")");
-		Python.finalize();
+		Python.initConfig(config.addressOf());
+		Python.setBytesString(config.addressOf(), config.program_name.toWChar(), Sys.args()[0]);
+
+		if (Python.exception(status) == 1) {
+			exception();
+		}
+
+		status = Python.initializeFromConfig(config.addressOf());
+
+		if (Python.exception(status) == 1) {
+			exception();
+		}
+
+		Python.configClear(config.addressOf());
+
+		Python.runSimpleString("from time import time,ctime\n" + "print('Today is', ctime(time()))\n");
+		if(Python.finalizeEx() < 0){
+			Sys.exit(120);
+		};
+	}
+
+	static function exception() {
+		Python.configClear(config.addressOf());
+		Python.exitStatusException(status);
 	}
 }
