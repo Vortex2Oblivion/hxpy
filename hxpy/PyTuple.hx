0774323f6c5c51dc81889c76d3ab1274c7425fae
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-04-26 15:08:43 -0400

    update python to 3.13.3

--- hxpy/PyTuple.hx
+++ hxpy/PyTuple.hx
@@ -1,9 +1,15 @@
 package hxpy;
 
+import cpp.Rest;
+import cpp.VarArg;
+
 extern class PyTuple {
 	@:native('PyTuple_New')
 	static function newPyTuple(size:Int):RawPointer<PyObject>;
 
 	@:native('PyTuple_SetItem')
 	static function setItem(p:RawPointer<PyObject>, pos:Int, o:RawPointer<PyObject>):Int;
+
+	@:native('PyTuple_Pack')
+	public static function pack(size:PySsizeT, args:Rest<VarArg>):RawPointer<PyObject>;
 }
--- hxpy/macros/GlobalMetadata.hx
+++ hxpy/macros/GlobalMetadata.hx
@@ -4,7 +4,7 @@ import haxe.macro.Compiler;
 
 class GlobalMetadata {
 	public static macro function run() {
-		Compiler.addGlobalMetadata('hxpy', "@:buildXml(\"<include name='${haxelib:hxpy}/hxpy/Build.xml' />\")");
+		Compiler.addGlobalMetadata('hxpy', "@:buildXml(\"<include name='${haxelib:hxpy}/build.xml' />\")");
 		Compiler.addGlobalMetadata('hxpy', '@:include("Python.h")');
 		Compiler.addGlobalMetadata('hxpy', '@:structAccess');
         return macro null;
