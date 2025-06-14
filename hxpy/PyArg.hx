897fd45591034e458427588af511e2692d3cf4ba
Vortex2Oblivion <73261680+Vortex2Oblivion@users.noreply.github.com>
2025-06-14 15:55:55 -0400

    update python to 3.13.5

--- hxpy/PyArg.hx
+++ hxpy/PyArg.hx
@@ -4,6 +4,14 @@ import cpp.VarArg;
 import cpp.Rest;
 
 extern class PyArg {
+	/**
+	 * [Description]
+	 * @param args 
+	 * @param format 
+	 * @param rest 
+	 * @return Bool
+	 * @see https://www.mit.edu/people/amliu/vrut/python/ext/parseTuple.html
+	 */
 	@:native('PyArg_ParseTuple')
 	static function parseTuple(args:RawPointer<PyObject>, format:ConstCharStar, rest:Rest<VarArg>):Bool;
 }
